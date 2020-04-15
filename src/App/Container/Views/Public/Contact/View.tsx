3794a8aacd857334fd60a22c63182353afa87186
arieazhari <iniarieazhari@gmail.com>
2020-04-16 01:18:34 +0700

    eprox-12 Contact

--- src/App/Container/Views/Public/Contact/View.tsx
+++ src/App/Container/Views/Public/Contact/View.tsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+import { MainApp } from "../../../Components/index";
 
 export const Contact = () => {
-    return (
-        <div>
-            Contact
+  return (
+    <MainApp title="CONTACT">
+      <div className="flex mb-4">
+        <div className="w-1/5"></div>
+        <div className="w-1/5 text-sm font-bold">Kantor Pusat</div>
+      </div>
+      <div className="flex mb-2">
+        <div className="w-1/5"></div>
+        <div className="w-1/5 text-xs font-bold">PT. Pindad</div>
+      </div>
+      <div className="flex mb-4">
+        <div className="w-1/5 text-xs text-right px-4 font-semibold">
+          Alamat
         </div>
-    )
-}
+        <div className="w-1/5 text-xs">
+          Jl. Gatot Subroto, No 517 Bandung, Indonesia, 40284
+        </div>
+      </div>
+      <div className="flex">
+        <div className="w-1/5 text-xs text-right px-4 font-semibold">Telp.</div>
+        <div className="w-1/5 text-xs">+62 22 7312073</div>
+      </div>
+      <div className="flex mb-10">
+        <div className="w-1/5 text-xs text-right px-4 font-semibold">Fax.</div>
+        <div className="w-1/5 text-xs">+62 22 7301222</div>
+      </div>
+      <div className="flex mb-4">
+        <div className="w-1/5"></div>
+        <div className="w-1/5 text-sm font-bold">Kantor Cabang</div>
+      </div>
+      <div className="flex mb-2">
+        <div className="w-1/5"></div>
+        <div className="w-1/5 text-xs font-bold">PT. Pindad</div>
+      </div>
+      <div className="flex mb-4">
+        <div className="w-1/5 text-xs text-right px-4 font-semibold">
+          Alamat
+        </div>
+        <div className="w-1/5 text-xs">Jl. Batu Ceper No. 28 Jakarta 10120</div>
+      </div>
+      <div className="flex">
+        <div className="w-1/5 text-xs text-right px-4 font-semibold">Telp.</div>
+        <div className="w-1/5 text-xs">+62 21 3806929</div>
+      </div>
+      <div className="flex">
+        <div className="w-1/5 text-xs text-right px-4 font-semibold">Fax.</div>
+        <div className="w-1/5 text-xs">+62 21 3814039</div>
+      </div>
+    </MainApp>
+  );
+};
