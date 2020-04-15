fc85cb9f7b2858e5832e94af957d8623b6eae9e8
arieazhari <iniarieazhari@gmail.com>
2020-04-16 03:43:27 +0700

    EProc-6 Integrity Fact/ no content

--- src/App/Container/Views/Public/IntegrityFact/View.tsx
+++ src/App/Container/Views/Public/IntegrityFact/View.tsx
@@ -1,5 +1,6 @@
 import React from "react";
+import { MainApp } from '../../../Components/index';
 
 export const IntegrityFact = () => {
-  return <div>IntegrityFact</div>;
+  return <MainApp title="FAKTA INTEGRITAS">Fakta Integritas</MainApp>;
 };
