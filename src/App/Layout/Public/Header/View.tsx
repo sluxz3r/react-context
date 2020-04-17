77c747540928d679630bb2722285d3ccddd25716
Fuad Rifki Refactory <fuad.rifki98@gmail.com>
2020-04-17 21:16:32 +0700

    fix

--- src/App/Layout/Public/Header/View.tsx
+++ src/App/Layout/Public/Header/View.tsx
@@ -378,10 +378,58 @@ export const PublicHeader = () => {
         </ul>
         <ul className="list-reset lg:flex justify-end flex-1 items-center">
           <li>
-            <span className="inline-block p-4 text-white text-xs font-bold hover:bg-bluepindad-200">
-              Bahasa
-            </span>
+            <div
+              className="cursor-pointer w-full md:w-full lg:w-auto block lg:inline-block border-none text-sm leading-none text-white  hover:bg-blue-800 lg:mt-0"
+              onClick={handleOpenLanguage}
+            >
+              <div className="flex flex-col">
+                <div className="flex flex-row items-center pl-4 pr-2 py-5">
+                  <span>Bahasa</span>
+                  {!openLanguage ? (
+                    <svg
+                      className="fill-current h-4 w-4 mr-2 ml-2"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 320 512"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
+                      ></path>
+                    </svg>
+                  ) : (
+                    <svg
+                      className="fill-current h-4 w-4 mr-2 ml-2"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 320 512"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M288.662 352H31.338c-17.818 0-26.741-21.543-14.142-34.142l128.662-128.662c7.81-7.81 20.474-7.81 28.284 0l128.662 128.662c12.6 12.599 3.676 34.142-14.142 34.142z"
+                      ></path>
+                    </svg>
+                  )}
+                </div>
+              </div>
+            </div>
           </li>
+          <div
+            className={`border border-gray-300 shadow-lg flex flex-col w-options lg:bg-white text-left static md:static lg:absolute text-white lg:text-black left-language rounded ${
+              !openLanguage && "hidden"
+            }`}
+          >
+            <div
+              className="cursor-pointer border-b border-gray-300 hover:bg-gray-400 py-2 px-4"
+              onClick={() => history.push("/")}
+            >
+              English
+            </div>
+            <div
+              className="cursor-pointer hover:bg-gray-400 py-2 px-4"
+              onClick={() => history.push("/")}
+            >
+              Bahasa
+            </div>
+          </div>
           {token === null && (
             <React.Fragment>
               <li>
@@ -414,15 +462,68 @@ export const PublicHeader = () => {
           >
             <Login />
           </Modal>
-          {token !== null && (
-            <li>
-              <span
-                onClick={() => _handleLogout()}
-                className="inline-block px-4 text-white text-xs"
+          {token === null && (
+            <React.Fragment>
+              <li>
+                <div
+                  className="cursor-pointer w-full md:w-full lg:w-auto block lg:inline-block border-none text-sm leading-none text-white  hover:bg-blue-800 lg:mt-0"
+                  onClick={handleOpenOptions}
+                >
+                  <div className="flex flex-col">
+                    <div className="flex flex-row items-center pl-4 pr-2 py-5">
+                      <span>User Tes</span>
+                      {!openOptions ? (
+                        <svg
+                          className="fill-current h-4 w-4 mr-2 ml-2"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 320 512"
+                        >
+                          <path
+                            fill="currentColor"
+                            d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
+                          ></path>
+                        </svg>
+                      ) : (
+                        <svg
+                          className="fill-current h-4 w-4 mr-2 ml-2"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 320 512"
+                        >
+                          <path
+                            fill="currentColor"
+                            d="M288.662 352H31.338c-17.818 0-26.741-21.543-14.142-34.142l128.662-128.662c7.81-7.81 20.474-7.81 28.284 0l128.662 128.662c12.6 12.599 3.676 34.142-14.142 34.142z"
+                          ></path>
+                        </svg>
+                      )}
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <div
+                className={`border border-gray-300 shadow-lg flex flex-col lg:bg-white text-left static md:static lg:absolute text-white lg:text-black border border-gray-300 rounded left-options ${
+                  !openOptions ? "hidden" : ""
+                }`}
               >
-                User Test
-              </span>
-            </li>
+                <div
+                  className="cursor-pointer font-bold hover:bg-gray-400 py-2 px-4"
+                  onClick={() => history.push("/")}
+                >
+                  Username
+                </div>
+                <div
+                  className="cursor-pointer border-t border-b border-gray-300 hover:bg-gray-400 py-2 px-4"
+                  onClick={() => history.push("/")}
+                >
+                  Ganti Password
+                </div>
+                <div
+                  className="cursor-pointer hover:bg-gray-400 py-2 px-4"
+                  onClick={() => _handleLogout()}
+                >
+                  Keluar
+                </div>
+              </div>
+            </React.Fragment>
           )}
         </ul>
       </div>
