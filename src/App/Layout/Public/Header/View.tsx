bd0f5bc948c30619af8a0a99ce248d16e70c6b43
Fuad Rifki Refactory <fuad.rifki98@gmail.com>
2020-04-17 17:33:13 +0700

    add prosedur dan pengumuman

--- src/App/Layout/Public/Header/View.tsx
+++ src/App/Layout/Public/Header/View.tsx
@@ -21,10 +21,58 @@ export const PublicHeader = () => {
   const _handleContact = () => {
     history.push("/contact");
   };
-  const [openGuidance, setOpenGuidance] = useState(false);
 
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleOpenModal = () => setOpenModal(true);
+
+  const handleClose = () => {
+    setOpenModal(false);
+  };
+
+  const [open, setOpen] = useState(false);
+  const [openProcedure, setOpenProcedure] = useState(false);
+  const [openGuidance, setOpenGuidance] = useState(false);
+  const [openAnnouncement, setOpenAnnoucement] = useState(false);
+  const [openLanguage, setOpenLanguage] = useState(false);
+  const [openOptions, setOpenOptions] = useState(false);
+
+  const handleOpen = () => setOpen(!open);
+  const handleOpenProcedure = () => {
+    setOpenProcedure(!openProcedure);
+    setOpenGuidance(false);
+    setOpenAnnoucement(false);
+    setOpenLanguage(false);
+    setOpenOptions(false);
+  };
   const handleOpenGuidance = () => {
     setOpenGuidance(!openGuidance);
+    setOpenProcedure(false);
+    setOpenAnnoucement(false);
+    setOpenLanguage(false);
+    setOpenOptions(false);
+  };
+  const handleOpenAnnouncement = () => {
+    setOpenAnnoucement(!openAnnouncement);
+    setOpenProcedure(false);
+    setOpenGuidance(false);
+    setOpenLanguage(false);
+    setOpenOptions(false);
+  };
+  const handleOpenLanguage = () => {
+    setOpenLanguage(!openLanguage);
+    setOpenProcedure(false);
+    setOpenGuidance(false);
+    setOpenAnnoucement(false);
+    setOpenOptions(false);
+  };
+
+  const handleOpenOptions = () => {
+    setOpenOptions(!openOptions);
+    setOpenProcedure(false);
+    setOpenGuidance(false);
+    setOpenAnnoucement(false);
+    setOpenLanguage(false);
   };
 
   const [open, setOpen] = useState(false);
@@ -214,14 +262,110 @@ export const PublicHeader = () => {
             </div>
           </li>
           <li>
-            <span className="inline-block p-4 text-white text-xs hover:bg-bluepindad-200">
-              Prosedur
-            </span>
+            <div
+              className="cursor-pointer w-full md:w-full lg:w-auto block lg:inline-block border-none text-sm leading-none text-white  hover:bg-blue-800 lg:mt-0"
+              onClick={handleOpenProcedure}
+            >
+              <div className="flex flex-col">
+                <div className="flex flex-row items-center pl-4 pr-2 py-5">
+                  <span>Prosedur</span>
+                  {!openProcedure ? (
+                    <svg
+                      className="fill-current h-4 w-4 mr-2 ml-2"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 320 512"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
+                      ></path>
+                    </svg>
+                  ) : (
+                    <svg
+                      className="fill-current h-4 w-4 mr-2 ml-2"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 320 512"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M288.662 352H31.338c-17.818 0-26.741-21.543-14.142-34.142l128.662-128.662c7.81-7.81 20.474-7.81 28.284 0l128.662 128.662c12.6 12.599 3.676 34.142-14.142 34.142z"
+                      ></path>
+                    </svg>
+                  )}
+                </div>
+                <div
+                  className={`border border-gray-300 shadow-lg flex flex-col w-options lg:bg-white text-left static md:static lg:absolute text-white lg:text-black top-options  rounded ${
+                    !openProcedure && "hidden"
+                  }`}
+                >
+                  <div
+                    className="cursor-pointer hover:bg-gray-400 py-2 px-4"
+                    onClick={() => history.push("/registration_procedure")}
+                  >
+                    Prosedur Registrasi
+                  </div>
+                  <div
+                    className="cursor-pointer hover:bg-gray-400 py-2 px-4"
+                    onClick={() => history.push("/qualification_requirement")}
+                  >
+                    Persyaratan Kualifikasi
+                  </div>
+                  <div
+                    className="cursor-pointer hover:bg-gray-400 py-2 px-4"
+                    onClick={() => history.push("/tender_requirement")}
+                  >
+                    Persyaratan Pembelian
+                  </div>
+                </div>
+              </div>
+            </div>
           </li>
           <li>
-            <span className="inline-block p-4 text-white text-xs hover:bg-bluepindad-200">
-              Pengumuman
-            </span>
+            <div
+              className="cursor-pointer w-full md:w-full lg:w-auto block lg:inline-block border-none text-sm leading-none text-white  hover:bg-blue-800 lg:mt-0"
+              onClick={handleOpenAnnouncement}
+            >
+              <div className="flex flex-col">
+                <div className="flex flex-row items-center pl-4 pr-2 py-5">
+                  <span>Pengumuman</span>
+                  {!openAnnouncement ? (
+                    <svg
+                      className="fill-current h-4 w-4 mr-2 ml-2"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 320 512"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
+                      ></path>
+                    </svg>
+                  ) : (
+                    <svg
+                      className="fill-current h-4 w-4 mr-2 ml-2"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 320 512"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M288.662 352H31.338c-17.818 0-26.741-21.543-14.142-34.142l128.662-128.662c7.81-7.81 20.474-7.81 28.284 0l128.662 128.662c12.6 12.599 3.676 34.142-14.142 34.142z"
+                      ></path>
+                    </svg>
+                  )}
+                </div>
+                <div
+                  className={`border border-gray-300 shadow-lg flex flex-col w-options lg:bg-white text-left static md:static lg:absolute text-white lg:text-black top-options  rounded ${
+                    !openAnnouncement && "hidden"
+                  }`}
+                >
+                  <div
+                    className="cursor-pointer hover:bg-gray-400 py-2 px-4"
+                    onClick={() => history.push("/procurements")}
+                  >
+                    Lelang
+                  </div>
+                </div>
+              </div>
+            </div>
           </li>
           <li>
             <span
