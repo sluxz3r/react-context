f543247410e307ac3aa0bd9baa4bd4aa9681c6d5
arieazhari <iniarieazhari@gmail.com>
2020-04-17 19:18:38 +0700

    fix edit

--- src/App/Layout/Private/Sidebar/View.tsx
+++ src/App/Layout/Private/Sidebar/View.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 export const PrivateSidebar = () => {
     return (
-        <div className="lg:w-32">
-            Private Sidebar
+        <div className="lg:h-full lg:w-64 bg-gray-600">
+            <div className="h-10 bg-gray-300">
+                test
+            </div>
+            <div>
+                test
+            </div>
         </div>
     )
 }
--- src/App/Layout/Public/Header/View.tsx
+++ src/App/Layout/Public/Header/View.tsx
@@ -4,20 +4,61 @@ import { getToken, removeAuthCredential } from "../../../Misc/Cookies";
 
 export const PublicHeader = () => {
   const token: any = getToken();
-  const _handleLogout =() => {
-    removeAuthCredential()
-    window.location.reload()
-  }  
-  const _handleHomeMenu = () => {
-    history.push("/");
-  };
-  const _handleRegistration = () => {
-    history.push("/registration");
+  const _handleLogout = () => {
+    removeAuthCredential();
+    window.location.reload();
   };
 
-  const _handleContact = () => {
-    history.push("/contact");
-  };
+  const _handleClickMenu = (e) => {
+    if (!e.isDropdown) {
+      history.push(e.path)
+    }
+  }
+  const menuLeft: any = [
+    {
+      id:1,
+      title: " e-Procurement",
+      path: "/",
+      isDropdown: false,
+    },
+    {
+      id:2,
+      title: "Beranda",
+      path: "/",
+      isDropdown: false,
+    },
+    {
+      id:3,
+      title: "Panduan",
+      isDropdown: true,
+    },
+    {
+      id:4,
+      title: "Prosedur",
+      isDropdown: true,
+    },
+    {
+      id:5,
+      title: "Pengumuman",
+      isDropdown: true,
+    },
+    {
+      id:6,
+      title: "Kontak",
+      path: "/contact",
+      isDropdown: false,
+    },
+  ];
+
+  const menuRight: any = [
+    {
+      id:7,
+      title: "Registrasi",
+      path: "/registration",
+      isDropdown: false,
+    },
+  ]
+
   return (
     <nav className="flex items-center justify-between flex-wrap bg-bluepindad-100 border-b-6 border-yellowpindad-100 py-0 px-6 fixed w-full z-10 top-0 left-0 right-0 overflow-hidden">
       <div
@@ -28,45 +69,13 @@ export const PublicHeader = () => {
           <li>
             <span className="inline-block px-8 text-white font-bold">Logo</span>
           </li>
-          <li>
-            <span
-              onClick={() => _handleHomeMenu()}
-              className="inline-block p-4 text-white font-bold hover:bg-bluepindad-200"
-            >
-              e-Procurement
-            </span>
-          </li>
-          <li>
-            <span
-              onClick={() => _handleHomeMenu()}
-              className="inline-block p-4 text-white text-xs hover:bg-bluepindad-200"
-            >
-              Beranda
-            </span>
-          </li>
-          <li>
-            <span className="inline-block p-4 text-white text-xs hover:bg-bluepindad-200">
-              Panduan
-            </span>
-          </li>
-          <li>
-            <span className="inline-block p-4 text-white text-xs hover:bg-bluepindad-200">
-              Prosedur
-            </span>
-          </li>
-          <li>
-            <span className="inline-block p-4 text-white text-xs hover:bg-bluepindad-200">
-              Pengumuman
-            </span>
-          </li>
-          <li>
-            <span
-              onClick={() => _handleContact()}
-              className="inline-block p-4 text-white text-xs hover:bg-bluepindad-200"
-            >
-              Kontak
-            </span>
-          </li>
+          {menuLeft.map((e, i) => (
+            <li key={i}>
+              <span onClick={() => _handleClickMenu(e)} className={`${e.id === 1 && 'font-bold'} inline-block p-4 text-white text-xs hover:bg-bluepindad-200`}>
+                {e.title}
+              </span>
+            </li>
+          ))}
         </ul>
         <ul className="list-reset lg:flex justify-end flex-1 items-center">
           <li>
@@ -76,14 +85,16 @@ export const PublicHeader = () => {
           </li>
           {token === null && (
             <React.Fragment>
-              <li>
+              {menuRight.map((e,i) => 
+              <li key={i}>
                 <span
-                  onClick={() => _handleRegistration()}
+                  onClick={() => _handleClickMenu(e)}
                   className="inline-block p-4 text-white text-xs hover:bg-bluepindad-200"
                 >
-                  Registrasi
+                  {e.title}
                 </span>
               </li>
+              )}
               <li>
                 <span className="inline-block px-4 text-white text-xs">
                   <button className="bg-blue-600 py-1 px-4 hover:bg-blue-700">
@@ -93,14 +104,16 @@ export const PublicHeader = () => {
               </li>
             </React.Fragment>
           )}
-          {token !==
-            null && (
-              <li>
-                <span onClick={() => _handleLogout()} className="inline-block px-4 text-white text-xs">
-                  User Test
-                </span>
-              </li>
-            )}
+          {token !== null && (
+            <li>
+              <span
+                onClick={() => _handleLogout()}
+                className="inline-block px-4 text-white text-xs"
+              >
+                User Test
+              </span>
+            </li>
+          )}
         </ul>
       </div>
     </nav>
