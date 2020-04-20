29f2ed34c02adeae31e0c24130388f6bc1da2c1c
Shola <Sholahuddinmufasy@gmail.com>
2020-04-20 11:48:22 +0700

    responsive tabel and filter

---
+++ src/App/Container/Views/Public/Procurement/Query.tsx
@@ -0,0 +1,12 @@
+// import gql from "graphql-tag";
+
+// export const GET_PROCUREMENT = gql`
+//   query {
+//     GetProcurement {
+//       id
+//       procurement
+//     }
+//   }
+// `;
+
+export const query = {};
