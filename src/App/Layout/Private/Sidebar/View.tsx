fd32d6195db0fe101438bbffdd5da56f9cbdee16
arieazhari <iniarieazhari@gmail.com>
2020-04-20 22:59:33 +0700

    eproc sidebar 35/ belum responsive

--- src/App/Layout/Private/Sidebar/View.tsx
+++ src/App/Layout/Private/Sidebar/View.tsx
@@ -73,28 +73,30 @@ export const PrivateSidebar = () => {
   const [childrenList, setChildrenList] = useState<any>(urlList);
 
   const _clickSidebar = (e) => {
-      setSidebarStatus(e)
-      setChildrenList(urlList)
-  }
+    setSidebarStatus(e);
+    setChildrenList(urlList);
+  };
   const _handleSidebar = (path, hasChildren, index, value) => {
-    if (!hasChildren) {
-      history.push(path);
-    } else {
-      if (value === false) {
-        let newArr = [...childrenList];
-        newArr[index].isOpen = true;
-        setChildrenList(newArr);
+    if (sidebarStatus) {
+      if (!hasChildren) {
+        history.push(path);
       } else {
-        let newArr = [...childrenList];
-        newArr[index].isOpen = false;
-        setChildrenList(newArr);
+        if (value === false) {
+          let newArr = [...childrenList];
+          newArr[index].isOpen = true;
+          setChildrenList(newArr);
+        } else {
+          let newArr = [...childrenList];
+          newArr[index].isOpen = false;
+          setChildrenList(newArr);
+        }
       }
     }
   };
 
   const _handleClickChild = (e) => {
-      history.push(e)
-  }
+    history.push(e);
+  };
 
   return (
     <div
@@ -140,27 +142,27 @@ export const PrivateSidebar = () => {
                   {e.hasChildren && (
                     <>
                       {!e.isOpen ? (
-                      <svg
-                        className="fill-current h-4 w-4 mr-2 ml-2"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 320 512"
-                      >
-                        <path
-                          fill="currentColor"
-                          d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
-                        ></path>
-                      </svg>
+                        <svg
+                          className="fill-current h-4 w-4 mr-2 ml-2"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 320 512"
+                        >
+                          <path
+                            fill="currentColor"
+                            d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
+                          ></path>
+                        </svg>
                       ) : (
-                      <svg
-                        className="fill-current h-4 w-4 mr-2 ml-2"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 320 512"
-                      >
-                        <path
-                          fill="currentColor"
-                          d="M288.662 352H31.338c-17.818 0-26.741-21.543-14.142-34.142l128.662-128.662c7.81-7.81 20.474-7.81 28.284 0l128.662 128.662c12.6 12.599 3.676 34.142-14.142 34.142z"
-                        ></path>
-                      </svg>
+                        <svg
+                          className="fill-current h-4 w-4 mr-2 ml-2"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 320 512"
+                        >
+                          <path
+                            fill="currentColor"
+                            d="M288.662 352H31.338c-17.818 0-26.741-21.543-14.142-34.142l128.662-128.662c7.81-7.81 20.474-7.81 28.284 0l128.662 128.662c12.6 12.599 3.676 34.142-14.142 34.142z"
+                          ></path>
+                        </svg>
                       )}
                     </>
                   )}
