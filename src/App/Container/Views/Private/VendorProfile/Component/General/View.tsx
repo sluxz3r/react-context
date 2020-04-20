2a97f1b024e19db7d09f97943e5fb125f64f3e20
Fuad Rifki Refactory <fuad.rifki98@gmail.com>
2020-04-20 23:01:29 +0700

    fix

--- src/App/Container/Views/Private/VendorProfile/Component/General/View.tsx
+++ src/App/Container/Views/Private/VendorProfile/Component/General/View.tsx
@@ -4,16 +4,7 @@ import { GeneralController } from "./Controller";
 
 const General = () => {
   return (
-<<<<<<< HEAD
     <GeneralController>
-=======
-    <MainApp
-      title="PROFIL PERUSAHAAN"
-      onActionRefresh={() => alert("Ok")}
-      onActionDone={() => alert("Done")}
-      onActionNew={() => alert("New")}
-    >
->>>>>>> add reusable 4 button main app
       <GeneralComponent />
     </GeneralController>
   );
